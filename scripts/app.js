e3ac879ec2bf3ccc6a73c62a778f3a2dc0d621c7
Ricardo <mvargas.ricardo@gmail.com>
2019-10-23 11:40:28 -0300

    Implementado toda a regra manipulação de status dos filtros de herois. Criada as regras para mudar o CSS dos botões de filtros por posições de acordo com o status selecionado.

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,100 @@
 (function() {
     "use strict";
-    /** Main app class.
-     * 
+    /** Constants with the states names e states values. */
+    const STATE_NAME = "filterHeroName";
+    const STATE_DIFFICULTY = "filterHeroDifficulty";
+    const STATE_ROLE = "filterHeroRole";
+    const ROLE_SUPPORT = "support";
+    const ROLE_TANK = "tank";
+    const ROLE_DAMAGE = "damage";
+    const DIFFICUTIES = [
+        { 
+            'display':  '-- select --',
+            'key': '0'
+        },
+        {
+            'display': 'Easy',
+            'key': 'easy'
+        },
+        {
+            'display': 'Moderate',
+            'key': 'moderate'
+        },
+        {
+            'display': 'Hard',
+            'key': 'hard'
+        }
+    ];
+    /** Class with filter opions for listing Heroes.
+     * Contains methods to update states filters.
      */
-    class HeroesApp extends React.Component{
-        constructor(props) {
-            super(props);
+    function HeroesFilter(props) {
+        const CSS_ROLE_IMG = "filter-img-damage d-block mx-auto";
+        const CSS_ROLE_NAME = "filter-role-name mx-auto text-center";
+        // Update filters states:
+        function updateFilterName(e) { 
+            props.updateStates(STATE_NAME, e.target.value);
         }
-        render() {
-            return (
-                <main className="container-fluid">
-                    <div className="text-center bg-secondary p-1">
-                        <img className="site-logo" src="img/overwatch_logo.png" alt="Overwatch" title="Overwacth" />
-                        <h1 className="font-bold text-warning">HEROES</h1>
-                    </div>
-                    <HeroesFilter />
-                    <HeroesList />
-                </main>
-            );
+        function updateFilterDifficulty(e) {
+            props.updateStates(STATE_DIFFICULTY, e.target.value);
+         }
+        function updateFilterRoleSupport() { 
+            props.updateStates(STATE_ROLE, ROLE_SUPPORT);
         }
-    }
-    /** Class with filter options for listing Heroes.
-     * 
-     */
-    class HeroesFilter extends React.Component {
-        constructor(props) {
-            super(props);
-            this.setState({
-                heroName: "",
-                heroDifficulty: "",
-                heroRole: ""
-            });
+        function updateFilterRoleTank() { 
+            props.updateStates(STATE_ROLE, ROLE_TANK);
         }
-        render(){
-            return(
-                <section id="filters-component">
-                    <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">FILTERS</h1>
-                    <div className="container-fluid">
-                        <ul className="row">
-                            <li className="col-sm-12 col-md-6">
-                                <div className="mx-auto filter-block">
-                                    <label htmlFor="filter-name">Hero name:</label>
-                                    <input id="filter-name" type="text" name="filter-name" />
-                                </div>
-                            </li>
-                            <li className="col-sm-12 col-md-6">
-                                <div className="mx-auto filter-block">
-                                    <label fohtmlForr="filter-diffitulty">Hero difficult:</label>
-                                    <select id="filter-difficulty" name="filter-difficulty">
-                                        <option value="0">-- select ---</option>
-                                        <option value="easy">Easy</option>
-                                        <option value="modarate">Moderate</option>
-                                        <option value="difficult">Difficult</option>
-                                    </select>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="container-fluid filter-roles">
-                        <ul className="row">
-                            <li className="col-4">
-                                <a href="#" class="filter-img-damage d-block mx-auto"></a>
-                                <p className="filter-role-name mx-auto text-center">DAMAGE</p>
-                            </li>
-                            <li className="col-4">
-                                <a href="#" className="filter-img-tank d-block mx-auto"></a>
-                                <p className="filter-role-name mx-auto text-center">TANK</p>
-                            </li>
-                            <li className="col-4">
-                                <a href="#" className="filter-img-support d-block mx-auto"></a>
-                                <p className="filter-role-name mx-auto text-center">SUPPORT</p>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-            );
+        function updateFilterRoleDamage() { 
+            props.updateStates(STATE_ROLE, ROLE_DAMAGE);
         }
+        function resetStates() { 
+            props.resetStates();
+        }
+        return(
+            <section id="filters-component">
+                <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">FILTERS</h1>
+                <div className="container-fluid">
+                    <ul className="row">
+                        <li className="col-sm-12 col-md-6">
+                            <div className="mx-auto filter-block">
+                                <label htmlFor="filter-name">Hero name:</label>
+                                <input id="filter-name" type="text" name="filter-name" value={props.filterHeroName} onChange={updateFilterName} />
+                            </div>
+                        </li>
+                        <li className="col-sm-12 col-md-6">
+                            <div className="mx-auto filter-block">
+                                <label htmlFor="filter-diffitulty">Hero difficult:</label>
+                                <select id="filter-difficulty" name="filter-difficulty" value={props.filterHeroDifficulty} onChange={updateFilterDifficulty}>
+                                    {DIFFICUTIES.map(function(difficulty){
+                                        return (<option value={difficulty.key} key={difficulty.key}>{difficulty.display}</option>);
+                                    })}
+                                </select>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div id="filters-roles" className="container-fluid filter-roles">
+                    <ul className="row">
+                        <li className="col-4">
+                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_IMG + " bg-damage" : CSS_ROLE_IMG} 
+                            value={props.filterHeroRole} onClick={updateFilterRoleDamage}></a>
+                            <p className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_NAME + " bg-damage font-bold text-white" : CSS_ROLE_NAME}>DAMAGE</p>
+                        </li>
+                        <li className="col-4">
+                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_IMG + " bg-tank" : CSS_ROLE_IMG} 
+                            value={props.filterHeroRole} onClick={updateFilterRoleTank}></a>
+                            <p className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_NAME + " bg-tank font-bold text-white" : CSS_ROLE_NAME}>TANK</p>
+                        </li>
+                        <li className="col-4">
+                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_IMG + " bg-support" : CSS_ROLE_IMG} 
+                            value={props.filterHeroRole} onClick={updateFilterRoleSupport}></a>
+                            <p className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_NAME + " bg-support font-bold text-white" : CSS_ROLE_NAME}>SUPPORT</p>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" onClick={resetStates}>Clean filters</a>
+            </section>
+        );
     }
     /** Class that display a list of Heroes.
      * 
@@ -83,13 +102,13 @@
     class HeroesList extends React.Component {
         constructor(props) {
             super(props);
-            this.setState({
+            this.state = {
                 heroesList: []
-            });
+            };
         }
         render() {
             return(
-                <section id="heroes-list-component" class="container-fluid p-0">
+                <section id="heroes-list-component" className="container-fluid p-0">
                     <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">HEROES</h1>
                     <Hero />
                 </section>
@@ -140,6 +159,51 @@
                         </ul>
                     </div>
                 </div>
+            );
+        }
+    }
+    /** Main app class.
+     * 
+     */
+    class HeroesApp extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {
+                filterHeroName: "",
+                filterHeroDifficulty: "",
+                filterHeroRole: ""
+            };
+            this.updateStates = this.updateStates.bind(this);
+            this.resetStates = this.resetStates.bind(this);
+        }
+        updateStates(name, value) {
+            this.setState({
+                [name]: value
+            });
+        }
+        resetStates(){
+            this.setState({
+                filterHeroName: "",
+                filterHeroDifficulty: "",
+                filterHeroRole: ""
+            });
+        }
+        render() {
+            return (
+                <main className="container-fluid">
+                    <div className="text-center bg-secondary p-1">
+                        <img className="site-logo" src="img/overwatch_logo.png" alt="Overwatch" title="Overwacth" />
+                        <h1 className="font-bold text-warning">HEROES</h1>
+                    </div>
+                    <HeroesFilter 
+                        updateStates={this.updateStates} 
+                        resetStates={this.resetStates}
+                        filterHeroName={this.state.filterHeroName}
+                        filterHeroDifficulty={this.state.filterHeroDifficulty}
+                        filterHeroRole={this.state.filterHeroRole}
+                    />
+                    <HeroesList />
+                </main>
             );
         }
     }
