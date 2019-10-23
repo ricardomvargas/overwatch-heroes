9fe1f009d36247e528d3fe6fbb2208a61c5ce332
Ricardo <mvargas.ricardo@gmail.com>
2019-10-23 20:24:39 -0200

    Adicionar o arquivo heroes.js que contém uma lista de herois com apenas dois herois no momento. Implementada a funcionalidade de listagem dos herois do arquivo heroes.js.

--- scripts/app.js
+++ scripts/app.js
@@ -58,15 +58,19 @@
                         <li className="col-sm-12 col-md-6">
                             <div className="mx-auto filter-block">
                                 <label htmlFor="filter-name">Hero name:</label>
-                                <input id="filter-name" type="text" name="filter-name" value={props.filterHeroName} onChange={updateFilterName} />
+                                <input id="filter-name" type="text" name="filter-name" 
+                                    value={props.filterHeroName} onChange={updateFilterName} />
                             </div>
                         </li>
                         <li className="col-sm-12 col-md-6">
                             <div className="mx-auto filter-block">
                                 <label htmlFor="filter-diffitulty">Hero difficult:</label>
-                                <select id="filter-difficulty" name="filter-difficulty" value={props.filterHeroDifficulty} onChange={updateFilterDifficulty}>
+                                <select id="filter-difficulty" name="filter-difficulty" 
+                                    value={props.filterHeroDifficulty} onChange={updateFilterDifficulty}>
                                     {DIFFICUTIES.map(function(difficulty){
-                                        return (<option value={difficulty.key} key={difficulty.key}>{difficulty.display}</option>);
+                                        return (
+                                            <option value={difficulty.key} key={difficulty.key}>{difficulty.display}</option>
+                                        );
                                     })}
                                 </select>
                             </div>
@@ -76,18 +80,21 @@
                 <div id="filters-roles" className="container-fluid filter-roles">
                     <ul className="row">
                         <li className="col-4">
-                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_IMG + " bg-damage" : CSS_ROLE_IMG} 
-                            value={props.filterHeroRole} onClick={updateFilterRoleDamage}></a>
+                            <a href="#filters-roles" 
+                                className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_IMG + " bg-damage" : CSS_ROLE_IMG} 
+                                value={props.filterHeroRole} onClick={updateFilterRoleDamage}></a>
                             <p className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_NAME + " bg-damage font-bold text-white" : CSS_ROLE_NAME}>DAMAGE</p>
                         </li>
                         <li className="col-4">
-                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_IMG + " bg-tank" : CSS_ROLE_IMG} 
-                            value={props.filterHeroRole} onClick={updateFilterRoleTank}></a>
+                            <a href="#filters-roles" 
+                                className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_IMG + " bg-tank" : CSS_ROLE_IMG} 
+                                value={props.filterHeroRole} onClick={updateFilterRoleTank}></a>
                             <p className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_NAME + " bg-tank font-bold text-white" : CSS_ROLE_NAME}>TANK</p>
                         </li>
                         <li className="col-4">
-                            <a href="#filters-roles" className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_IMG + " bg-support" : CSS_ROLE_IMG} 
-                            value={props.filterHeroRole} onClick={updateFilterRoleSupport}></a>
+                            <a href="#filters-roles" 
+                                className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_IMG + " bg-support" : CSS_ROLE_IMG} 
+                                value={props.filterHeroRole} onClick={updateFilterRoleSupport}></a>
                             <p className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_NAME + " bg-support font-bold text-white" : CSS_ROLE_NAME}>SUPPORT</p>
                         </li>
                     </ul>
@@ -99,68 +106,47 @@
     /** Class that display a list of Heroes.
      * 
      */
-    class HeroesList extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                heroesList: []
-            };
-        }
-        render() {
-            return(
-                <section id="heroes-list-component" className="container-fluid p-0">
-                    <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">HEROES</h1>
-                    <Hero />
-                </section>
-            );
-        }
-    }
-    /** Class that display a Hero detail.
-     * TO-DO: Check if is necessary to be a class or if can just be a fuction.
-     */
-    class Hero extends React.Component {
-        constructor(props){
-            super(props);
-            // propos: name, role, difficulty, habilitiesList [name, img, description] 
-        }
-        render() {
-            return (
-                <div className="row hero-component">
-                    <div className="col-12 col-lg-3 col-xl-2 text-center">
-                        <h2>Ana</h2>
-                        <img className="hero-portrait border border-dark bg-support" src="img/ana/portrait.png" title="Ana" alt="Ana" />
-                    </div>
-                    <div className="col-12 col-lg-2 order-md-2 order-lg-3 text-center pt-1 pt-lg-0">
-                        <img className="img-icon-roles mr-2" src="img/support_icon_small.png" title="Support" alt="Support" />
-                        <img src="img/hard_icon.png" title="Hard" alt="Hard" />
-                    </div>
-                    <div className="col-12 col-lg-7 col-xl-8 order-md-3 order-lg-2 align-self-md-center">
-                        <ul>
-                            <li>
-                                <h2 className="bg-secondary text-white p-1">Biotic Rifle</h2>
-                                <img className="float-left mr-2 mb-2 bg-not-selected" src="img/ana/biotic_rifle.png" title="Biotic Fifle" alt="Biotic Rifle" />
-                                <p>O rifle de Ana atira dardos que podem restaurar a vida de seus aliados ou causar dano continuado a seus inimigos. Ela é capaz de usar a mira telescópica de seu rifle para dar tiros incrivelmente precisos.</p>
-                            </li>
-                            <li className="clearfix">
-                                <h2 className="bg-secondary text-white p-1">Sleep Dart</h2>
-                                <img className="float-left mr-2 mb-2 bg-not-selected img-skill" src="img/ana/sleep_dart.png" title="Sleep Dart" alt="Sleep Dart" />
-                                <p>Ana dispara um dardo de sua arma secundária, deixando seus inimigos inconscientes (porém eles despertam ao receber dano).</p>
-                            </li>
-                            <li className="clearfix">
-                                <h2 className="bg-secondary text-white p-1">Biotic Granade</h2>
-                                <img className="float-left mr-2 mb-2 bg-not-selected img-skill" src="img/ana/biotic_granade.png" title="Biotic Granade" alt="Biotic Granade" />
-                                <p>Ana lança uma granada biótica que causa dano aos inimigos e cura os aliados em uma pequena área de efeito. Aliados afetados recebem por tempo limitado cura aumentada de todas as fontes, enquanto inimigos pegos pela explosão não podem se curar por alguns instantes.</p>
-                            </li>
-                            <li className="clearfix">
-                                <h2 className="bg-secondary text-white p-1">Nano Boost</h2>
-                                <img className="float-left mr-2 mb-2 bg-not-selected img-skill" src="img/ana/nano_boost.png" title="Nano Boost" alt="Nano Boost" />
-                                <p>Depois que Ana atinge seus aliados com um estimulante de combate, eles causam mais dano e recebem menos dano dos ataques inimigos temporariamente.</p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        }
+    function HeroesList(props) {
+        return(
+            <section id="heroes-list-component" className="container-fluid p-0">
+                <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">HEROES</h1>
+                {props.heroesList.map(function(hero) {
+                    return(
+                        <div className="row hero-component">
+                            <div className="col-12 col-lg-3 col-xl-2 text-center">
+                                <h2>{hero.apelido}</h2>
+                                <img className={"hero-portrait border border-dark bg-" + hero.posicao.toLowerCase()}
+                                    src={"img/" + hero.apelido.toLowerCase() + "/portrait.png"}     
+                                    title={hero.nome} alt={hero.nome} />
+                            </div>
+                            <div className="col-12 col-lg-2 order-md-2 order-lg-3 text-center pt-1 pt-lg-0">
+                                <img className="img-icon-roles mr-2" 
+                                    src={"img/" + hero.posicao.toLowerCase() + "_icon_small.png"} 
+                                    title={hero.posicao} alt={hero.posicao} />
+                                <img src={"img/" + hero.dificuldade.toLowerCase() + "_icon.png"} 
+                                    title={hero.dificuldade} alt={hero.dificuldade} />
+                            </div>
+                            <div className="col-12 col-lg-7 col-xl-8 order-md-3 order-lg-2 align-self-md-center">
+                                <ul>
+                                    {hero.habilidades.map(function(hb) {
+                                        var hbDetails = hb.split('|');
+                                        return(
+                                            <li className="clearfix">
+                                                <h2 className="bg-secondary text-white p-1">{hbDetails[0]}</h2>
+                                                <img className="float-left mr-2 mb-2 bg-not-selected" 
+                                                    src={"img/" + hero.apelido.toLowerCase() + "/" + hbDetails[2] + ".png"} 
+                                                    title={hbDetails[0]} alt={hbDetails[0]} />
+                                                <p>{hbDetails[1]}</p>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    );
+                })}
+            </section>
+        );
     }
     /** Main app class.
      * 
@@ -168,13 +154,16 @@
     class HeroesApp extends React.Component{
         constructor(props) {
             super(props);
+            // Set the default states. In case of the list, the default is heroes.
             this.state = {
                 filterHeroName: "",
                 filterHeroDifficulty: "",
-                filterHeroRole: ""
+                filterHeroRole: "",
+                heroesList: HEROES
             };
             this.updateStates = this.updateStates.bind(this);
             this.resetStates = this.resetStates.bind(this);
+            //this.updateList = this.updateList.bind(this);
         }
         updateStates(name, value) {
             this.setState({
@@ -185,9 +174,23 @@
             this.setState({
                 filterHeroName: "",
                 filterHeroDifficulty: "",
-                filterHeroRole: ""
+                filterHeroRole: ""//,
+                //heroesList = this.heroes
             });
         }
+        /*updateList(list) {
+            var list = heroes.filter(
+                function(hero) {
+                    return (
+                        (this.state.filterHeroName === "" || hero.nome.toLowerCase().indexOf(this.state.filterHeroName.toLowerCase()) !== -1) 
+                        && 
+                        (this.state.filterHeroRole === "" || hero.posicao === this.state.filterHeroRole)
+                    );
+                }.bind(this));
+            this.setState({
+                heroesList: list
+            })
+        }*/
         render() {
             return (
                 <main className="container-fluid">
@@ -202,7 +205,7 @@
                         filterHeroDifficulty={this.state.filterHeroDifficulty}
                         filterHeroRole={this.state.filterHeroRole}
                     />
-                    <HeroesList />
+                    <HeroesList heroesList={this.state.heroesList}/>
                 </main>
             );
         }
