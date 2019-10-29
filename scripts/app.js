65c2218bf5cc4893ae4d9cacebacb41607297044
Ricardo <mvargas.ricardo@gmail.com>
2019-10-29 12:52:26 -0300

    Atualizando arquivos de dados dos herois e modificado leitura de dados de acordo com as alterações dos dados.

--- scripts/app.js
+++ scripts/app.js
@@ -25,12 +25,13 @@
             'key': 'hard'
         }
     ];
-    /** Class with filter opions for listing Heroes.
+    /** Function with filter opions for listing Heroes.
      * Contains methods to update states filters.
      */
     function HeroesFilter(props) {
-        const CSS_ROLE_IMG = "filter-img-damage d-block mx-auto";
-        const CSS_ROLE_NAME = "filter-role-name mx-auto text-center";
+        var css_bg_damage = props.filterHeroRole === ROLE_DAMAGE ? "bg-damage" : "";
+        var css_bg_tank = props.filterHeroRole === ROLE_TANK ? "bg-tank" : "";
+        var css_bg_support = props.filterHeroRole === ROLE_SUPPORT ? "bg-support" : "";
         // Update filters states:
         function updateFilterName(e) { 
             props.updateStates(STATE_NAME, e.target.value);
@@ -81,21 +82,21 @@
                     <ul className="row">
                         <li className="col-4">
                             <a href="#filters-roles" 
-                                className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_IMG + " bg-damage" : CSS_ROLE_IMG} 
+                                className={"filter-img-damage d-block mx-auto " + css_bg_damage}
                                 value={props.filterHeroRole} onClick={updateFilterRoleDamage}></a>
-                            <p className={props.filterHeroRole === ROLE_DAMAGE ? CSS_ROLE_NAME + " bg-damage font-bold text-white" : CSS_ROLE_NAME}>DAMAGE</p>
+                            <p className={"filter-role-name mx-auto text-center " + css_bg_damage}>DAMAGE</p>
                         </li>
                         <li className="col-4">
                             <a href="#filters-roles" 
-                                className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_IMG + " bg-tank" : CSS_ROLE_IMG} 
+                                className={"filter-img-tank d-block mx-auto " + css_bg_tank}
                                 value={props.filterHeroRole} onClick={updateFilterRoleTank}></a>
-                            <p className={props.filterHeroRole === ROLE_TANK ? CSS_ROLE_NAME + " bg-tank font-bold text-white" : CSS_ROLE_NAME}>TANK</p>
+                            <p className={"filter-role-name mx-auto text-center " + css_bg_tank}>TANK</p>
                         </li>
                         <li className="col-4">
                             <a href="#filters-roles" 
-                                className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_IMG + " bg-support" : CSS_ROLE_IMG} 
+                                className={"filter-img-support d-block mx-auto " + css_bg_support}
                                 value={props.filterHeroRole} onClick={updateFilterRoleSupport}></a>
-                            <p className={props.filterHeroRole === ROLE_SUPPORT ? CSS_ROLE_NAME + " bg-support font-bold text-white" : CSS_ROLE_NAME}>SUPPORT</p>
+                            <p className={"filter-role-name mx-auto text-center " + css_bg_support}>SUPPORT</p>
                         </li>
                     </ul>
                 </div>
@@ -103,54 +104,54 @@
             </section>
         );
     }
-    /** Class that display a list of Heroes.
-     * 
-     */
+    /** Function that display a list of Heroes.*/
     function HeroesList(props) {
         return(
             <section id="heroes-list-component" className="container-fluid p-0">
                 <h1 className="font-weight-bold text-center bg-dark text-warning pt-2 pb-2">HEROES</h1>
                 {props.heroesList.map(function(hero) {
-                    return(
-                        <div className="row hero-component">
-                            <div className="col-12 col-lg-3 col-xl-2 text-center">
-                                <h2>{hero.apelido}</h2>
-                                <img className={"hero-portrait border border-dark bg-" + hero.posicao.toLowerCase()}
-                                    src={"img/" + hero.apelido.toLowerCase() + "/portrait.png"}     
-                                    title={hero.nome} alt={hero.nome} />
-                            </div>
-                            <div className="col-12 col-lg-2 order-md-2 order-lg-3 text-center pt-1 pt-lg-0">
-                                <img className="img-icon-roles mr-2" 
-                                    src={"img/" + hero.posicao.toLowerCase() + "_icon_small.png"} 
-                                    title={hero.posicao} alt={hero.posicao} />
-                                <img src={"img/" + hero.dificuldade.toLowerCase() + "_icon.png"} 
-                                    title={hero.dificuldade} alt={hero.dificuldade} />
-                            </div>
-                            <div className="col-12 col-lg-7 col-xl-8 order-md-3 order-lg-2 align-self-md-center">
-                                <ul>
-                                    {hero.habilidades.map(function(hb) {
-                                        var hbDetails = hb.split('|');
-                                        return(
-                                            <li className="clearfix">
-                                                <h2 className="bg-secondary text-white p-1">{hbDetails[0]}</h2>
-                                                <img className="float-left mr-2 mb-2 bg-not-selected" 
-                                                    src={"img/" + hero.apelido.toLowerCase() + "/" + hbDetails[2] + ".png"} 
-                                                    title={hbDetails[0]} alt={hbDetails[0]} />
-                                                <p>{hbDetails[1]}</p>
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            </div>
-                        </div>
-                    );
+                    return(<Hero hero={hero} />);
                 })}
             </section>
         );
     }
-    /** Main app class.
-     * 
-     */
+    /** Function that display a hero. */
+    function Hero(props) {
+        return(
+            <div className="row hero-component">
+                <div className="col-12 col-lg-3 col-xl-2 text-center">
+                    <h2>{props.hero.apelido}</h2>
+                    <img className={"hero-portrait border border-dark bg-" + props.hero.posicao.toLowerCase()}
+                        src={"img/" + props.hero.apelido.toLowerCase() + "/portrait.png"}     
+                        title={props.hero.nome} alt={props.hero.nome} />
+                </div>
+                <div className="col-12 col-lg-2 order-md-2 order-lg-3 text-center pt-1 pt-lg-0">
+                    <img className="img-icon-roles mr-2" 
+                        src={"img/" + props.hero.posicao.toLowerCase() + "_icon_small.png"} 
+                        title={props.hero.posicao} alt={props.hero.posicao} />
+                    <img src={"img/" + props.hero.dificuldade.toLowerCase() + "_icon.png"} 
+                        title={props.hero.dificuldade} alt={props.hero.dificuldade} />
+                </div>
+                <div className="col-12 col-lg-7 col-xl-8 order-md-3 order-lg-2 align-self-md-center">
+                    <ul>
+                        {props.hero.habilidades.map(function(hb) {
+                            var hbDetails = hb.split('|');
+                            return(
+                                <li className="clearfix">
+                                    <h2 className="bg-secondary text-white p-1">{hbDetails[0]}</h2>
+                                    <img className="float-left mr-2 mb-2 bg-not-selected" 
+                                        src={"img/" + props.hero.apelido.toLowerCase() + "/" + hbDetails[2] + ".png"} 
+                                        title={hbDetails[0]} alt={hbDetails[0]} />
+                                    <p>{hbDetails[1]}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+    /** Main app class. */
     class HeroesApp extends React.Component{
         constructor(props) {
             super(props);
@@ -163,34 +164,34 @@
             };
             this.updateStates = this.updateStates.bind(this);
             this.resetStates = this.resetStates.bind(this);
-            //this.updateList = this.updateList.bind(this);
         }
         updateStates(name, value) {
             this.setState({
                 [name]: value
-            });
+            }, this.updateList);
         }
         resetStates(){
             this.setState({
                 filterHeroName: "",
                 filterHeroDifficulty: "",
-                filterHeroRole: ""//,
-                //heroesList = this.heroes
-            });
+                filterHeroRole: ""
+            }, this.updateList);
         }
-        /*updateList(list) {
-            var list = heroes.filter(
+        updateList() {
+            var list = HEROES.filter(
                 function(hero) {
                     return (
-                        (this.state.filterHeroName === "" || hero.nome.toLowerCase().indexOf(this.state.filterHeroName.toLowerCase()) !== -1) 
+                        (this.state.filterHeroName === "" || hero.apelido.toLowerCase().indexOf(this.state.filterHeroName.toLowerCase()) !== -1) 
                         && 
-                        (this.state.filterHeroRole === "" || hero.posicao === this.state.filterHeroRole)
+                        (this.state.filterHeroRole === "" || hero.posicao.toLowerCase() === this.state.filterHeroRole)
+                        &&
+                        (this.state.filterHeroDifficulty === "" || hero.dificuldade.toLowerCase() === this.state.filterHeroDifficulty)
                     );
                 }.bind(this));
             this.setState({
                 heroesList: list
             })
-        }*/
+        }
         render() {
             return (
                 <main className="container-fluid">
